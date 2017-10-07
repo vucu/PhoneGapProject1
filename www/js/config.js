1201bab2b9e8055056d2c9ed88523fc87c2056fe
Vu Cu <cxv@g.ucla.edu>
2017-10-07 11:04:27 +0700

    First commit

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={				
+    'ApiUrl': "https://www.mewokrom.com/merchantapp/api",
+	'DialogDefaultTitle' : "Mewokrom Merchant",
+	'pushNotificationSenderid': "947875450243",
+	'APIHasKey': ""
+};
