4e3032aa13cacae9ddce1ab3fcf67faa48047619
Vu Cu <cxv@g.ucla.edu>
2017-10-08 07:45:52 +0700

    Reset

--- platforms/browser/www/js/config.js
+++
@@ -1,6 +0,0 @@
-var krms_config ={				
-    'ApiUrl': "https://www.mewokrom.com/merchantapp/api",
-	'DialogDefaultTitle' : "Mewokrom Merchant",
-	'pushNotificationSenderid': "947875450243",
-	'APIHasKey': ""
-};
